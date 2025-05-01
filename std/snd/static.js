afd7a7a22b1a8bb317d926eac979e957eeddecb9
Lingdong H <lingdong0618@hotmail.com>
2025-05-01 02:11:21 -0400

    js snd

--- std/snd/static.js
+++ std/snd/static.js
@@ -1,9 +1,42 @@
 globalThis.$snd = new function(){
   let that = this;
+
+  let workletCode = `
+    class MyProcessor extends AudioWorkletProcessor {
+      constructor(){
+        super();
+        this.queue = [];
+        this.port.onmessage = (e) => {
+          for (let i = 0; i < e.data.length; i++){
+            this.queue.push(e.data[i]);
+          }
+        };
+      }
+      process(inputs, outputs){
+        const output = outputs[0];
+        const frames = output[0].length;
+        let idx = 0;
+        for (let i = 0; i < frames; i++){
+          for (let c = 0; c < output.length; c++){
+            output[c][i] = idx < this.queue.length ? this.queue[idx++] : 0.0;
+          }
+        }
+        this.queue.splice(0,idx);
+        if (this.queue.length <= frames){
+          this.port.postMessage(1);
+        }
+        return true;
+      }
+    }
+    registerProcessor('my-processor', MyProcessor);
+  `;
+
+  
   let BUF_SIZE = 512;
-  let audioSource, audioCtx;
-  let rate=44100,chan=2;
-  let inp_buf,out_buf,abuf_idx,abuf_ch,audio_die;
+  let buffer = [];
+
+  let audioCtx, workletNode;
+  let chan,rate;
   let did_act = 0;
   let want_init = 0;
   let btn;
@@ -19,82 +52,69 @@ globalThis.$snd = new function(){
     if (ignore.includes(e.key)) return;
     act_init();
   });
-  // let t0 = new Date().getTime();
   function animation_frame() {
-    // let t = new Date().getTime();
-    // let dt = t-t0;
-    // t0 = t;
-    // console.log(dt);
     return new Promise(resolve => requestAnimationFrame(resolve));
   }
   that.init = async function(){
     ;[rate,chan] = $pop_args(2);
     btn = document.createElement("button");
-    // btn.innerHTML = "start audio context"
-    // btn.style = "position:absolute; left:calc(50% - 75px); top:calc(50% - 10px); width:150px;"
     btn.innerHTML = "📢"
     btn.style = "position:absolute;"
     document.body.appendChild(btn);
-    btn.onclick = function(){
+    btn.onclick = async function(){
       want_init = 0;
       btn.parentElement.removeChild(btn);
-      audioCtx = new AudioContext({sampleRate:rate});
-      let buf0 = audioCtx.createBuffer(chan, BUF_SIZE, rate);
-      let buf1 = audioCtx.createBuffer(chan, BUF_SIZE, rate);
-      rate = rate;
-      chan = chan;
-      inp_buf = buf0;
-      out_buf = buf1;
-      abuf_idx = 0;
-      abuf_ch = 0;
-      audio_die = 0;
-      function flipper(){
-        if (audio_die) return;
-        if (abuf_idx >= BUF_SIZE){
-          [out_buf,inp_buf] = [inp_buf,out_buf];
-          abuf_idx = 0;
-          abuf_ch = 0;
-        }
-        audioSource = audioCtx.createBufferSource();
-        audioSource.buffer = out_buf;
-        audioSource.onended = flipper;
-        audioSource.connect( audioCtx.destination );
-        audioSource.start();
+      let _audioCtx = new AudioContext({ sampleRate: rate });
+      const blob = new Blob([workletCode], { type: 'application/javascript' });
+      const url = URL.createObjectURL(blob);
+      await _audioCtx.audioWorklet.addModule(url);
+      workletNode = new AudioWorkletNode(_audioCtx, 'my-processor', {
+        outputChannelCount: [chan]
+      });
+      workletNode.connect(_audioCtx.destination);
+      audioCtx = _audioCtx;
+
+      workletNode.port.onmessage = function(){
+        workletNode.port.postMessage(buffer);
+        buffer = []
       }
-      flipper();
+
     }
     if (did_act){
-      btn.click();
+      await btn.onclick();
     }else{
       want_init = 1;
     }
     await animation_frame();
-  }
+  };
+
+  that.put_sample = async function(){
+    let [x] = $pop_args(1);
+    if (!audioCtx){
+      await animation_frame();
+      return;
+    }
+    buffer.push(x);
+  };
+
   that.buffer_full = async function(){
-    if (!audioSource){
+    if (!audioCtx){
       await animation_frame();
       return 1;
     }
-    let cd = inp_buf.getChannelData(chan-1);
-    let o = abuf_idx >= cd.length;
+    
+    let o = buffer.length >= BUF_SIZE;
     if (o){
       await animation_frame();
     }
     return o;
-  }
-  that.put_sample = function(){
-    if (!audioSource) return;
-    let [x] = $pop_args(1);
-    let cd = inp_buf.getChannelData(abuf_ch);
-    cd[abuf_idx++] = x;
-    abuf_ch = (abuf_ch + 1) % chan;
-  }
-  that.exit = function(){
-    if (!audioSource) return;
-    audio_die = 1;
-    audioCtx.close()
-  }
-
-}
+  };
 
 
+  that.exit = async function(){
+
+    if (workletNode) workletNode.disconnect();
+    if (audioCtx) audioCtx.close();
+  
+  };
+};
